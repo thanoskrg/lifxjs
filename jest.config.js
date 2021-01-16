0e28a64976c5523294718a92a272f3906cda4f83
Thanos Karagiannis <thanoskrg@gmail.com>
2021-01-16 18:08:16 +0200

    chore: fix eslint issue

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 const { LIFX_API } = require('./config.json');
 
 module.exports = {
-  "moduleNameMapper": {
-    "lifxjs": "<rootDir>/index.js",
-    "fixtures/(.*)$": "<rootDir>/tests/fixtures/$1",
+  moduleNameMapper: {
+    lifxjs: '<rootDir>/index.js',
+    'fixtures/(.*)$': '<rootDir>/tests/fixtures/$1'
   },
-  "globals": {
+  globals: {
     /* sample token */
-    APP_TOKEN: 'c52826c87adfa1aa5cc85c87df245e2afdd4bb6c361687bd29869432470cc68d',
+    APP_TOKEN:
+      'c52826c87adfa1aa5cc85c87df245e2afdd4bb6c361687bd29869432470cc68d',
     LIFX_API_URL: LIFX_API.BASE_URL + '/v' + LIFX_API.VERSION
   }
 };
